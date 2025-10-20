a10a9983780ddc7efb28509c046c167121f4d206
whitewolfoo <archerschoepp@gmail.com>
2025-10-19 21:08:50 -0600

    Im not sure how this works so I hope this works

---
+++ src/main/java/net/whitewolfoo/AncientIron/ancientiron.java
@@ -0,0 +1,13 @@
+package net.whitewolfoo.AncientIron;
+
+
+import net.neoforged.fml.common.Mod;
+
+
+
+@Mod(ancientiron.MODID)
+public class ancientiron {
+
+    public static final String MODID = "wolfsancientiron";
+
+    }
